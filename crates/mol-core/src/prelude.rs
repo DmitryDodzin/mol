2f5f97ece86a03c7af4939312fc4f4e5a928130a
Dmitry Dodzin <d.dodzin@gmail.com>
2021-12-14 23:53:13 +0200

    feat: move some functions from mol-cargo to mol-core (#27)
    * feat: move some functions from mol-cargo to mol-core
    
    * chore: changeset
    
    * chore: rename Explorer function names

--- crates/mol-core/src/prelude.rs
+++ crates/mol-core/src/prelude.rs
@@ -3,6 +3,7 @@ pub use crate::changelog::{AsChangelogFmt, Changelog};
 pub use crate::changeset::Changeset;
 pub use crate::changesets::Changesets;
 pub use crate::error::{ChangesetParseError, ExplorerError, VersionParseError};
+pub use crate::explorer::Explorer;
 pub use crate::package::{AsPackageGraph, Package, PackageGraph};
 pub use crate::package_manager::PackageManager;
 pub use crate::plugin::{Plugin, PluginContext, PluginManager, PluginProxy, PluginRegistrar};
